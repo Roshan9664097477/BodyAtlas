a88bb903ec6d51ef514fb1f6c32345f4a9e23946
Roshan9664097477 <magicroshan007@gmail.com>
2025-12-06 13:03:58 +0530

    Update groqApiKey in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  groqApiKey: '',
+  groqApiKey: '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',
   apiUrl: 'http://localhost:4200/api',
   googleClientId: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com'
 };
